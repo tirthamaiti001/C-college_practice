f2778438c92bfd4c8c19b1084a5df865c14ea41d
Tirtha Maiti <tirtha.maiti.2006@gmail.com>
2025-11-10 18:52:42 +0530

    Question 02.c

---
+++ Lab Experiment 01/Q2.c
@@ -0,0 +1,15 @@
+/* Print the pattern using \n and \t in C language:
+     *
+  *     *
+*    *     *
+*/
+#include<stdio.h>
+
+int main()
+{
+  printf("\t\t*\n");
+  printf("\t*\t\t*\n");
+  printf("*\t\t*\t\t*\n");
+  
+  return 0;
+}
