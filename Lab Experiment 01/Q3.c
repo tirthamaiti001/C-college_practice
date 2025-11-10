be8761ff365f4533fbce23e6e33e511611c1e3c4
Tirtha Maiti <tirtha.maiti.2006@gmail.com>
2025-11-10 19:04:55 +0530

    Question 03

---
+++ Lab Experiment 01/Q3.c
@@ -0,0 +1,20 @@
+//Write a program to calculate the sum, difference, product, quotient and reminder of two given numbers.
+#include <stdio.h>
+
+int main()
+{
+  int numl=10, num2 = 5, a,b,c,d,e;
+  a = num1 + num2;
+  b = num1 - num2;
+  c = num1 * num2;
+  d = num1 / num2;
+  e = num1 % num2;
+  
+  printf("Sum of two numbers = %d\n",a);
+  printf("Difference between two = %d\n",b);
+  printf("Product of two numbers = %d\n",c);
+  printf("Quotient of two numbers = %d\n",d);
+  printf("Remainder of the division = %d\n",e);
+  
+  return 0;
+}
