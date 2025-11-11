bb94a784da43698c4dfed98cabb912fbeeeceaac
Tirtha Maiti <tirtha.maiti.2006@gmail.com>
2025-11-11 20:33:19 +0530

    Question 01

---
+++ Lab Experiment 02/Q9.c
@@ -0,0 +1,20 @@
+//Write a C language to check weather a person is eligible to vote.
+#include<stdio.h>
+
+int main()
+{
+  int age;
+  printf("Enter your age: ");
+  scanf("%d", &age);
+  
+  if (age >= 18)
+  {
+    printf ("You are eligible to vote.\n");
+  }
+  else
+  {
+    printf("You are not eligible to vote!\n");
+  }
+  
+  return 0;
+}
